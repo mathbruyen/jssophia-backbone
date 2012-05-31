e09ea5b8315c92673e61bd3b37e03613d3504274
Mathieu Bruyen <code@mais-h.eu>
2012-05-31 13:09:21 +0200

    Several bug fixes on the display

--- js/backbone-localstorage.js
+++ js/backbone-localstorage.js
@@ -64,7 +64,7 @@ _.extend(Store.prototype, {
 
 // Override `Backbone.sync` to use delegate to the model or collection's
 // *localStorage* property, which should be an instance of `Store`.
-Backbone.sync = function(method, model, options) {
+LocalStorageSync = function(method, model, options) {
 
   var resp;
   var store = model.localStorage || model.collection.localStorage;
